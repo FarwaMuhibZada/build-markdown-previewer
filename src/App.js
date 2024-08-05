cd8e73eed09f487a5db8eeb7e7921cee02ad434b
farwaMuhibzada <farwafarid2017@gmail.com>
2024-08-05 22:19:17 +0430

    added the text area and a div for previwe text in app.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+// src/App.js
+import React, { useState } from 'react';
 import './App.css';
+const initialMarkdown = `# Heading
+## Sub-heading
+[Link](https://example.com)
+\`Inline code\`
+\`\`\`
+Code block
+\`\`\`
+- List item
+> Blockquote
+![Image](https://via.placeholder.com/150)
+**Bold text**
+`;
 
 function App() {
+  const [markdown, setMarkdown] = useState(initialMarkdown);
+
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
   return (
     <div className="App">
+      <textarea id="editor" value={markdown} onChange={handleChange}></textarea>
+      <div id="preview">
+        {markdown}
+      </div>
     </div>
   );
 }
 
 export default App;
+
