7b15aecbf76584b470b356c9c90e7551917e311b
farwaMuhibzada <farwafarid2017@gmail.com>
2024-08-11 21:20:05 +0430

    refeactor the code and make a markdown previwer components .

--- src/App.js
+++ src/App.js
@@ -1,44 +1,9 @@
-// src/App.js
-import React, { useState } from 'react';
-import { marked } from 'marked';
 import './App.css';
-
-const initialMarkdown = `# Heading
-## Sub-heading
-[Link](https://example.com)
-\`Inline code\`
-\`\`\`
-Code block
-\`\`\`
-- List item
-> Blockquote
-![Image](https://via.placeholder.com/150)
-**Bold text**
-`;
-
+import MarkdownPreviwer from './components/MarkdownPreviwer';
 function App() {
-  const [markdown, setMarkdown] = useState(initialMarkdown);
-
-  const handleChange = (e) => {
-    setMarkdown(e.target.value);
-  };
-
-  marked.setOptions({
-    breaks: true,
-  });
-
   return (
     <>
-      <div id="heading">
-        <h1>Markdown Previewer</h1>
-      </div>
-      <div className="App">
-        <textarea id="editor" value={markdown} onChange={handleChange} />
-        <div
-          id="preview"
-          dangerouslySetInnerHTML={{ __html: marked(markdown) }}
-        />
-      </div>
+     <MarkdownPreviwer />
     </>
   );
 }
---
+++ src/components/MarkdownPreviwer.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { marked } from 'marked';
+
+const initialMarkdown = `# Heading
+## Sub-heading
+[Link](https://example.com)
+\`Inline code\`
+\`\`\`
+Code block
+\`\`\`
+- List item
+> Blockquote
+![Image](https://via.placeholder.com/150)
+**Bold text**
+`;
+
+const MarkdownPreviwer = () => {
+  const [markdown, setMarkdown] = useState(initialMarkdown);
+
+  const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  };
+
+  marked.setOptions({
+    breaks: true,
+  });
+
+  return (
+    <>
+      <div id="heading">
+        <h1>Markdown Previewer</h1>
+      </div>
+      <div className="App">
+        <textarea id="editor" value={markdown} onChange={handleChange} />
+        <div
+          id="preview"
+          dangerouslySetInnerHTML={{ __html: marked(markdown) }}
+        />
+      </div>
+    </>
+  );
+
+
+}
+export default MarkdownPreviwer;
